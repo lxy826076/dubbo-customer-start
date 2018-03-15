7aec6fe8e115a502f60b243eb0bfa9afed6303a6
lxy <1041305262@qq.com>
2018-03-15 18:21:31 +0800

    项目初始化

---
+++ src/main/java/com/dubbo/consumer/ConsumerApplication.java
@@ -0,0 +1,28 @@
+package com.dubbo.consumer;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.alibaba.dubbo.provider.DemoService;
+
+public class ConsumerApplication {
+    public static void main(String[] args) throws Exception {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                new String[]{"classpath:dubbo-consumer.xml"});
+        context.start();
+        // obtain proxy object for remote invocation
+        DemoService demoService = (DemoService) context.getBean("demoService");
+        // execute remote invocation
+        int i = 0;
+        while (true) {
+			String hello = demoService.sayHello("world");
+			System.out.println(hello);
+			System.out.println("---------------");;
+			i++;
+			if (i>5) {
+				break;
+			}
+		}
+        
+        Thread.sleep(1000*60*3);
+    }
+}
