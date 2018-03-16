3c52cd0db70e1f0696af29b78c154a52866df4df
lxy <1041305262@qq.com>
2018-03-16 11:08:13 +0800

    添加get set 方法

--- src/main/java/com/dubbo/bean/Student.java
+++ src/main/java/com/dubbo/bean/Student.java
@@ -3,5 +3,28 @@ package com.dubbo.bean;
 public class Student {
 	private String name;
 	private String sex;
-	private String age;;
+	private String age;
+	
+	public Student() {
+		super();
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
 }
